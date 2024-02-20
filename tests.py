e74d894d8a3179f04b57d4de6b712e5a37ecd16f
MarkJ <122132921+Mark-J34@users.noreply.github.com>
2024-02-20 08:55:54 -0800

    Update tests.py
    Attempted to fix linting errors

--- tests.py
+++ tests.py
@@ -7,11 +7,11 @@ class TestCase(unittest.TestCase):
     def test1(self):
         expected = "Hello World"
         self.assertEqual(task.my_func(), expected)
-        
 
     def test2(self):
         expected = "Hola World"
         self.assertEqual(task.my_func(), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
